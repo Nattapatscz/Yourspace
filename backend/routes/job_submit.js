29f1ea4725349ab3200deed69eab5bee300a6828
Nattapatscz <s6404062630333@email.kmutnb.ac.th>
2023-10-08 17:39:11 +0700

    reset flie

--- backend/routes/job_submit.js
+++ backend/routes/job_submit.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const db = require("./db");
+const multer = require("multer");
 
-router.post("/submit-job", (req, res) => {
+// กำหนดโฟลเดอร์ที่จะเก็บรูปภาพ
+const upload = multer({ dest: "uploads/" });
+
+router.post("/submit-job", upload.single("image"), (req, res) => {
   const {
     job_tel,
     job_details,
@@ -16,9 +20,11 @@ router.post("/submit-job", (req, res) => {
     technicial_username,
   } = req.body;
 
+  const image = req.file ? req.file.filename : null; // หากมีรูปภาพถูกอัปโหลดให้ใช้ชื่อไฟล์
+
   // Insert data into the job table
-  const query = `INSERT INTO job (job_tel, job_details, job_assign_date, job_assign_time, job_backup_tel, job_location, member_username, status_id, job_type_id, technicial_username) 
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const query = `INSERT INTO job (job_tel, job_details, job_assign_date, job_assign_time, job_backup_tel, job_location, member_username, status_id, job_type_id, technicial_username, image) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
   db.query(
     query,
     [
@@ -32,6 +38,7 @@ router.post("/submit-job", (req, res) => {
       status_id,
       job_type_id,
       technicial_username,
+      image,
     ],
     (err, result) => {
       if (err) {
