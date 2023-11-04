bacf7b5ef7624b3c631b81ba958a5ff0d305efef
Nattapatscz <s6404062630333@email.kmutnb.ac.th>
2023-11-05 00:14:27 +0700

    add docker

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,11 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 // middleware
+
 app.use(cors());
+
+app.use(express.static('build')); // ใช้เอกสารสถานที่เชื่อมต่อ
+
 app.use(express.json());
 app.use(fileUpload());
 
